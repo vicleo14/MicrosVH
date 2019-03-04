4142be0236249d65ac7a2b88efc0625a38653f27
vicleo14 <vicleo.morales@hotmail.com>
2019-03-04 04:28:20 -0600

    Primeros programas Micros

---
+++ programa1.X/build/default/production/PRINCIPAL.o.d
@@ -0,0 +1,6 @@
+build/default/production/PRINCIPAL.o:  \
+PRINCIPAL.S 
+build/default/production/PRINCIPAL.o:  \
+./p30F4013.inc  \
+  \
+PRINCIPAL.S  \
---
+++ programa5.X/build/default/debug/PRINCIPAL.o.d
@@ -0,0 +1,6 @@
+build/default/debug/PRINCIPAL.o:  \
+PRINCIPAL.S 
+build/default/debug/PRINCIPAL.o:  \
+./p30F4013.inc  \
+  \
+PRINCIPAL.S  \
